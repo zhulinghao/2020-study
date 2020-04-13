5fd4b00ae7ef2f17d99d7f6a4404d27888b076c5
祝令昊 <linghao.zhu@e-melt.com>
2020-04-13 22:06:22 +0800

    4-13

---
+++ my-vue/myVue.js
@@ -0,0 +1,125 @@
+const compileUtil = {
+  getVal(expr, vm) {
+    return expr.split('.').reduce((data, currentVal) => {
+      return data[currentVal];
+    }, vm.$data);
+  },
+  text(node,expr,vm) {
+    let value;
+    if(expr.indexOf('{{') > -1) {
+      value = expr.replace(/\{\{(.+?)\}\}/g, (...args) => {
+        return this.getVal(args[1], vm);
+      });
+    } else {
+      value = this.getVal(expr, vm);
+    }
+
+    this.updater.textUpdater(node, value);
+  },
+  html(node,expr,vm) {
+    const value = this.getVal(expr, vm);
+    this.updater.htmlUpdater(node, value);
+  },
+  model(node,expr,vm) {
+    const value = this.getVal(expr, vm);
+    this.updater.modelUpdater(node, value);
+  },
+  on(node,expr,vm,eventName) {
+    let fn = vm.$options.methods[expr];
+    node.addEventListener(eventName, fn);
+  },
+  updater: {
+    modelUpdater(node, value) {
+      node.value = value;
+    },
+    textUpdater(node, value) {
+      node.textContent = value;
+    },
+    htmlUpdater(node, value) {
+      node.innerHTML = value;
+    }
+  }
+}
+
+
+class Compile {
+  constructor(el, vm) {
+    this.el = this.isElementNode(el) ? el : document.querySelector(el);
+    this.vm = vm;
+    // 1 把node放到文档碎片当中，数据修改了，才修改dom，防止回流重绘影响性能
+    const fragment = this.node2Fragment(this.el);
+    // 2 编译模板
+    this.compile(fragment);
+    // 3 追加子元素到根元素
+    this.el.appendChild(fragment);
+  }
+
+  compile(fragment) {
+    const childNodes = fragment.childNodes;
+    [...childNodes].forEach(child => {
+      if(this.isElementNode(child)) {
+        // 是元素节点， 需要编译
+        this.compileElement(child);
+      } else {
+        // 文本节点，编译
+        this.compileText(child);
+      }
+
+      if(child.childNodes && child.childNodes.length) {
+        this.compile(child);
+      }
+    })
+  }
+  compileElement(node) {
+    // console.log('element',node);
+    const attributes = node.attributes;
+    [...attributes].forEach(attr => {
+      const { name, value } = attr;
+      if(this.isDirective(name)) {
+        const [,directive] = name.split('-');  // text on:click
+        const [dirName, eventName] = directive.split(":"); // [text, undefined] [on, click]
+        // 更新数据，数据驱动视图
+        compileUtil[dirName](node, value, this.vm, eventName);
+        // 删除标签上的属性
+        node.removeAttribute(name);
+      }
+    });
+  }
+  compileText(node) {
+    // console.log('text', node);
+    const content = node.textContent;
+
+    if(/\{\{(.+?)\}\}/.test(content)) {
+      compileUtil['text'](node, content, this.vm);
+    }
+
+  }
+  node2Fragment(el) {
+    // 创建文档碎片
+    const f = document.createDocumentFragment();
+    let firstChild;
+    while(firstChild = el.firstChild) {
+      f.appendChild(firstChild);
+    }
+    return f;
+  }
+  isDirective(attrName) {
+    return attrName.startsWith('v-');
+  }
+  // 是否是元素节点对象
+  isElementNode(node) {
+    return node.nodeType === 1;
+  }
+}
+
+class MyVue {
+  constructor(options) {
+    this.$el = options.el;
+    this.$data = options.data;
+    this.$options = options;
+    if(this.$el) {
+      // 解析器
+      new Compile(this.$el, this)
+    }
+  }
+}
